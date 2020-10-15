18b8e589ae44b515ff40f7346547f55e29480990
Rafael Garibotti <67865142+rafaelgaribotti@users.noreply.github.com>
2020-10-15 08:33:24 -0300

    Update TestFoo_Runner.c

--- examples/foo/test/test_runners/TestFoo_Runner.c
+++ examples/foo/test/test_runners/TestFoo_Runner.c
@@ -3,16 +3,6 @@
 
 TEST_GROUP_RUNNER(Foo)
 {
-    RUN_TEST_CASE(Foo, test_foo1);
-    RUN_TEST_CASE(Foo, test_foo2);
-}
-
-static void RunAllTests(void)
-{
-    RUN_TEST_GROUP(Foo);
-}
-
-int main(int argc, const char * argv[])
-{
-    return UnityMain(argc, argv, RunAllTests);
+  RUN_TEST_CASE(Foo, TestFoo1);
+  RUN_TEST_CASE(Foo, TestFoo2);
 }
